9b3a2713b80b0153081ac159216dc2111ef1c796
Laura Tannahill <lntannahill@gmail.com>
2021-01-06 13:05:20 -0400

    Add basic tbl layout to admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PW']
 
   def show
+    @numOfProducts = Product.count
+    @numOfCategories = Category.count
   end
 end
