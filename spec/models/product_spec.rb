ed14b34716cc7beebce71a3954859cb2dd03f88e
Laura Tannahill <lntannahill@gmail.com>
2021-01-11 20:19:59 +0000

    complete rspec testing for products

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,6 +2,44 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    # validation tests/examples here
+
+    it 'it is valid when all fields are present' do
+      @category = Category.new(name: "Dinosaur")
+      @category.save
+      @category.products.create(name: "T-rex", price: 1000, quantity: 3)
+      @category.save
+      expect(@category).to be_valid
+    end
+
+    it 'is not valid without a name' do
+      @category = Category.new(name: "Dinosaur")
+      @category.save
+      @category.products.create(name: nil, price: 1000, quantity: 3)
+      @category.save
+      expect(@category.errors.messages).to include(:products=>["is invalid"])
+    end
+
+    it 'is not valid without a price' do
+      @category = Category.new(name: "Dinosaur")
+      @category.save
+      @category.products.create(name: "T-rex", price: nil, quantity: 3)
+      @category.save
+      expect(@category.errors.messages).to include(:products=>["is invalid"])
+    end
+
+    it 'is not valid without a quantity' do
+      @category = Category.new(name: "Dinosaur")
+      @category.save
+      @category.products.create(name: "T-rex", price: 1000, quantity: nil)
+      @category.save
+      expect(@category.errors.messages).to include(:products=>["is invalid"])
+    end
+
+    it 'is not valid without a category' do
+      @product = Product.new(name: "T-rex", price: 1000, quantity: 1)
+      @product.save
+      expect(@product.errors.messages).to include(:category=>["can't be blank"])
+    end
+
   end
 end
