33c89ccdb9178d5ee5b5b60be5c439e263336ef1
Laura Tannahill <lntannahill@gmail.com>
2021-01-11 22:10:18 +0000

    Remove logic from controller and place into model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    if !User.exists?(email: user_params["email"])
-      user = User.new(user_params)
-      if user.save
-        session[:user_id] = user.id
-        redirect_to '/'
-      else
-        redirect_to '/signup'
-      end
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to '/'
     else
       redirect_to '/signup'
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  authenticate_with_credentials
   validates :email, uniqueness: { case_sensitive: false }, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :password, length: { in: 6..20 }
   validates :password_confirmation, length: { in: 6..20 }
-  has_secure_password
 end
