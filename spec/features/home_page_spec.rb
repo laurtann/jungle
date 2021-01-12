0c3e2e0f07d7fed2c1e880a1311c612af87b38c2
Laura Tannahill <lntannahill@gmail.com>
2021-01-12 17:08:14 +0000

    Create rspec files for home page & add class to product card header for testing

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -26,7 +26,7 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
 
     expect(page).to have_css 'article.product', count: 10
 
-    # DEBUG
+    # DEBUG - pass it a param and it'll save as that file name
     save_screenshot
 
     # VERIFY
